8ac8680616126b0d4c0faf9d539f6a172439014f
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-04-21 20:56:11 +0300

    feat: delate template file, add component in header; style: add style nav and linkNav

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,62 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+const showMenu = ref(true);
+const navBarShow = ref(true)
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <button class="closeButton" @click="showMenu = !showMenu">
+        <img v-if="showMenu" src="@/components/icons/menu.png" alt="menu" class="headerIcon"/>
+        <img v-else src="@/components/icons/icon-5.png" alt="close" class="headerIcon"/>
+      </button>
+      <nav class="navBar">
+        <RouterLink to="/" class="navLink">Главная</RouterLink>
+        <RouterLink to="/about" class="navLink">Информация о тесте</RouterLink>
+        <RouterLink to="/test" class="navLink">пройти тест</RouterLink>
       </nav>
-    </div>
   </header>
-
   <RouterView />
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  min-height: 100vh;
+  background: #181818;
+  display: block;
+  flex-direction: column;
 }
-
+.closeButton{
+  width: 28px;
+  height: 28px;
+  margin-top: 13px;
+}
+.headerIcon{
+  width: 28px;
+  height: 28px;
+}
+.navBar {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 22px;
+  text-transform: uppercase;
+  padding-top: 110px;
+  padding-left: 23px;
+}
+.navLink{
+  color: white;
+  padding-bottom: 27px;
+}
+.navLink:hover{
+  color: #F4CE0C;
+}
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: #F4CE0C;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
